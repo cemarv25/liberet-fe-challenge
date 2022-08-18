853f99d3f2fe8f704c737edd11fc57206d9468f4
cemarv25 <cesar.martinez0010@outlook.com>
2022-08-18 13:50:11 -0600

    added bottom bar

--- components/bottomBar.tsx
+++ components/bottomBar.tsx
@@ -1,3 +1,51 @@
 export default function BottomBar() {
-  return <h1>Bottom Bar</h1>;
+  return (
+    <div
+      className="flex justify-evenly items-center py-4"
+      style={{ backgroundColor: '#d1d1d1' }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-14 w-14"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#c1c1c1"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-14 w-14"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#e5b26f"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-14 w-14"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#c1c1c1"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+        />
+      </svg>
+    </div>
+  );
 }
